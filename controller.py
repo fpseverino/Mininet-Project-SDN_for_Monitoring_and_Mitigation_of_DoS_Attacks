741917bbbfdedb988cf01c2734cc548216c2fdff
fpseverino <francescopaolo.severino@gmail.com>
2025-06-24 14:08:00 +0000

    Format

--- controller.py
+++ controller.py
@@ -33,20 +33,19 @@ GREEN = "\033[92m"
 RESET = "\033[0m"
 
 
-
 class SimpleSwitch13(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     global timeInterval
     global RED
     global RESET
-    global GREEN 
+    global GREEN
 
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch13, self).__init__(*args, **kwargs)
         self.send_req = 0
         self.rec_res = 0
-        self.threshold=700000 #80-90% del percorso critico
+        self.threshold = 700000  # 80-90% del percorso critico
         self.time = 0
         self.datapaths = {}
         self.mac_to_port = {}
@@ -62,16 +61,16 @@ class SimpleSwitch13(app_manager.RyuApp):
         datapath = ev.datapath
         if ev.state == MAIN_DISPATCHER:
             if datapath.id not in self.datapaths:
-                self.logger.debug('register datapath: %016x', datapath.id)
+                self.logger.debug("register datapath: %016x", datapath.id)
                 self.datapaths[datapath.id] = datapath
         elif ev.state == DEAD_DISPATCHER:
             if datapath.id in self.datapaths:
-                self.logger.debug('unregister datapath: %016x', datapath.id)
+                self.logger.debug("unregister datapath: %016x", datapath.id)
                 del self.datapaths[datapath.id]
 
     # Funzione di richiesta delle stats agli switch
     def _request_stats(self, datapath):
-        self.logger.debug('send stats request: %016x', datapath.id)
+        self.logger.debug("send stats request: %016x", datapath.id)
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
@@ -85,7 +84,9 @@ class SimpleSwitch13(app_manager.RyuApp):
         while True:
             for dp in self.datapaths.values():
                 self._request_stats(dp)
-            self.logger.info('\n#########################################################')
+            self.logger.info(
+                "\n#########################################################"
+            )
             hub.sleep(timeInterval)
 
     # Funzione di stampa alla ricezione delle stats
@@ -107,127 +108,200 @@ class SimpleSwitch13(app_manager.RyuApp):
         """
 
         if ev.msg.datapath.id not in self.monitoring_stats.keys():
-            self.logger.info('datapath         port     '
-                             'rx-pkts   rx-bytes/s   rx-error   '
-                             'tx-pkts   tx-bytes/s   tx-error')
-            self.logger.info('---------------- --------   '
-                             '-------    --------   --------   '
-                             '--------   --------   --------')
-            for stat in sorted(body, key=attrgetter('port_no')):
-                self.logger.info('%016x %8d   %8d   %8d    %8d    %8d    %8d   %8d',
-                                 ev.msg.datapath.id, stat.port_no,
-                                 stat.rx_packets, stat.rx_bytes / self.time, stat.rx_errors,
-                                 stat.tx_packets, stat.tx_bytes / self.time, stat.tx_errors)
+            self.logger.info(
+                "datapath         port     "
+                "rx-pkts   rx-bytes/s   rx-error   "
+                "tx-pkts   tx-bytes/s   tx-error"
+            )
+            self.logger.info(
+                "---------------- --------   "
+                "-------    --------   --------   "
+                "--------   --------   --------"
+            )
+            for stat in sorted(body, key=attrgetter("port_no")):
+                self.logger.info(
+                    "%016x %8d   %8d   %8d    %8d    %8d    %8d   %8d",
+                    ev.msg.datapath.id,
+                    stat.port_no,
+                    stat.rx_packets,
+                    stat.rx_bytes / self.time,
+                    stat.rx_errors,
+                    stat.tx_packets,
+                    stat.tx_bytes / self.time,
+                    stat.tx_errors,
+                )
 
             self.monitoring_stats[ev.msg.datapath.id] = {
-                stat.port_no: [stat.rx_packets, stat.rx_bytes, stat.rx_errors, stat.tx_packets, stat.tx_bytes, stat.tx_errors]
-                for stat in sorted(body, key=attrgetter('port_no'))
+                stat.port_no: [
+                    stat.rx_packets,
+                    stat.rx_bytes,
+                    stat.rx_errors,
+                    stat.tx_packets,
+                    stat.tx_bytes,
+                    stat.tx_errors,
+                ]
+                for stat in sorted(body, key=attrgetter("port_no"))
             }
 
-            #Inizializzazione della struttura di alarm, questo sarà un contatore, a 3 (dopo 30 secondi) scatterà l'allarme per quella porta.
-            self.alarm_switch_port[ev.msg.datapath.id] = {stat.port_no: [0, 0] for stat in sorted(body, key=attrgetter('port_no'))}
+            # Inizializzazione della struttura di alarm, questo sarà un contatore, a 3 (dopo 30 secondi) scatterà l'allarme per quella porta.
+            self.alarm_switch_port[ev.msg.datapath.id] = {
+                stat.port_no: [0, 0] for stat in sorted(body, key=attrgetter("port_no"))
+            }
 
         else:
             previous = self.monitoring_stats[ev.msg.datapath.id]
-            self.logger.info('datapath         port     '
-                             'rx-pkts   rx-bytes/s   rx-error   '
-                             'tx-pkts   tx-bytes/s   tx-error')
-            self.logger.info('---------------- --------    '
-                             '--------   --------   --------   '
-                             '--------   --------   --------')
-            for stat in sorted(body, key=attrgetter('port_no')):
-                self.logger.info('%016x %8x   %8d   %8d   %8d   %8d   %8d   %8d',
-                                 ev.msg.datapath.id, stat.port_no,
-                                 (stat.rx_packets - previous[stat.port_no][0]),
-                                 (stat.rx_bytes - previous[stat.port_no][1]) / self.time,
-                                 stat.rx_errors - previous[stat.port_no][2],
-                                 (stat.tx_packets - previous[stat.port_no][3]),
-                                 (stat.tx_bytes - previous[stat.port_no][4]) / self.time,
-                                 stat.tx_errors - previous[stat.port_no][5])
-               
-			#gestione del contatore Alarm	
-                if  (((stat.rx_bytes - previous[stat.port_no][1]) / self.time) > self.threshold  or  ((stat.tx_bytes - previous[stat.port_no][4]) / self.time) > self.threshold):
-                
-                    if  self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][0] < 3: #Se per 30 secondi la threshold è superata, allora allarma. 
-                    
-                        self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][0] = self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][0] + 1
-                else:
-                    if  self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][0] > 0:
-                    
-                        self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][0] = self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][0] - 1
-				
-				
-				
-				
-                if  self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][0] == 3: #blocco della porta
-                    print(RED + "ALLARME SULLA PORTA " + str(stat.port_no) + " dello Switch " + str(ev.msg.datapath.id) + RESET)
-                   
-                    self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][1] =  1 
-                    
-                    print(self.alarm_switch_port)
-                    
-                    #BLOCCARE FLUSSO
-                    time.sleep(1)
-                    
-                    self.lock_flow(ev, stat.port_no)
-                    
-                elif self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][0] == 2 and self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][1] == 1:
-                	print( RED + "ALLARME SULLA PORTA " + str(stat.port_no) + " dello Switch " + str(ev.msg.datapath.id) + RESET)
-                	
-                	
-                elif self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][0] == 1 and self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][1] == 1 : #sblocco della porta 
-                    self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][1] = 0 
-                    self.unlock_flow(ev, stat.port_no)
-                    
-                
- 
-                
-                    
+            self.logger.info(
+                "datapath         port     "
+                "rx-pkts   rx-bytes/s   rx-error   "
+                "tx-pkts   tx-bytes/s   tx-error"
+            )
+            self.logger.info(
+                "---------------- --------    "
+                "--------   --------   --------   "
+                "--------   --------   --------"
+            )
+            for stat in sorted(body, key=attrgetter("port_no")):
+                self.logger.info(
+                    "%016x %8x   %8d   %8d   %8d   %8d   %8d   %8d",
+                    ev.msg.datapath.id,
+                    stat.port_no,
+                    (stat.rx_packets - previous[stat.port_no][0]),
+                    (stat.rx_bytes - previous[stat.port_no][1]) / self.time,
+                    stat.rx_errors - previous[stat.port_no][2],
+                    (stat.tx_packets - previous[stat.port_no][3]),
+                    (stat.tx_bytes - previous[stat.port_no][4]) / self.time,
+                    stat.tx_errors - previous[stat.port_no][5],
+                )
 
+                # gestione del contatore Alarm
+                if (
+                    (stat.rx_bytes - previous[stat.port_no][1]) / self.time
+                ) > self.threshold or (
+                    (stat.tx_bytes - previous[stat.port_no][4]) / self.time
+                ) > self.threshold:
+
+                    if (
+                        self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][0] < 3
+                    ):  # Se per 30 secondi la threshold è superata, allora allarma.
+
+                        self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][0] = (
+                            self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][0]
+                            + 1
+                        )
+                else:
+                    if self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][0] > 0:
+
+                        self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][0] = (
+                            self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][0]
+                            - 1
+                        )
+
+                if (
+                    self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][0] == 3
+                ):  # blocco della porta
+                    print(
+                        RED
+                        + "ALLARME SULLA PORTA "
+                        + str(stat.port_no)
+                        + " dello Switch "
+                        + str(ev.msg.datapath.id)
+                        + RESET
+                    )
+
+                    self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][1] = 1
+
+                    print(self.alarm_switch_port)
+
+                    # BLOCCARE FLUSSO
+                    time.sleep(1)
+
+                    self.lock_flow(ev, stat.port_no)
+
+                elif (
+                    self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][0] == 2
+                    and self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][1] == 1
+                ):
+                    print(
+                        RED
+                        + "ALLARME SULLA PORTA "
+                        + str(stat.port_no)
+                        + " dello Switch "
+                        + str(ev.msg.datapath.id)
+                        + RESET
+                    )
+
+                elif (
+                    self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][0] == 1
+                    and self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][1] == 1
+                ):  # sblocco della porta
+                    self.alarm_switch_port[ev.msg.datapath.id][stat.port_no][1] = 0
+                    self.unlock_flow(ev, stat.port_no)
 
             self.monitoring_stats[ev.msg.datapath.id] = {
-                stat.port_no: [stat.rx_packets, stat.rx_bytes, stat.rx_errors, stat.tx_packets, stat.tx_bytes, stat.tx_errors]
-                for stat in sorted(body, key=attrgetter('port_no'))
+                stat.port_no: [
+                    stat.rx_packets,
+                    stat.rx_bytes,
+                    stat.rx_errors,
+                    stat.tx_packets,
+                    stat.tx_bytes,
+                    stat.tx_errors,
+                ]
+                for stat in sorted(body, key=attrgetter("port_no"))
             }
 
-           
+    # Remediation per l'allarme
 
+    def lock_flow(self, ev, port_no):
 
-#Remediation per l'allarme
-
-    def lock_flow (self, ev, port_no):
-	
         ofproto = ev.msg.datapath.ofproto
         parser = ev.msg.datapath.ofproto_parser
-		
-        match = parser.OFPMatch(in_port=port_no) 
-		
-        instructions=[]
-		
-        flow_mod = parser.OFPFlowMod(datapath=ev.msg.datapath, priority=2, match=match, instructions=instructions, command=ofproto.OFPFC_ADD, out_port= ofproto.OFPP_ANY, out_group = ofproto.OFPG_ANY, flags=ofproto.OFPFF_SEND_FLOW_REM)
-		
-        ev.msg.datapath.send_msg(flow_mod)
-        print(RED + "Blocked traffic on port %s of switch %s " + RESET, port_no, ev.msg.datapath.id) 
-	
 
+        match = parser.OFPMatch(in_port=port_no)
+
+        instructions = []
+
+        flow_mod = parser.OFPFlowMod(
+            datapath=ev.msg.datapath,
+            priority=2,
+            match=match,
+            instructions=instructions,
+            command=ofproto.OFPFC_ADD,
+            out_port=ofproto.OFPP_ANY,
+            out_group=ofproto.OFPG_ANY,
+            flags=ofproto.OFPFF_SEND_FLOW_REM,
+        )
+
+        ev.msg.datapath.send_msg(flow_mod)
+        print(
+            RED + "Blocked traffic on port %s of switch %s " + RESET,
+            port_no,
+            ev.msg.datapath.id,
+        )
 
     def unlock_flow(self, ev, port_no):
-	
+
         ofproto = ev.msg.datapath.ofproto
         parser = ev.msg.datapath.ofproto_parser
-        
-        match = parser.OFPMatch(in_port=port_no) 
-        
-        flow_mod = parser.OFPFlowMod(datapath=ev.msg.datapath, priority=2, match=match, command=ofproto.OFPFC_DELETE, out_port= ofproto.OFPP_ANY, out_group = ofproto.OFPG_ANY, flags=ofproto.OFPFF_SEND_FLOW_REM)
-        
+
+        match = parser.OFPMatch(in_port=port_no)
+
+        flow_mod = parser.OFPFlowMod(
+            datapath=ev.msg.datapath,
+            priority=2,
+            match=match,
+            command=ofproto.OFPFC_DELETE,
+            out_port=ofproto.OFPP_ANY,
+            out_group=ofproto.OFPG_ANY,
+            flags=ofproto.OFPFF_SEND_FLOW_REM,
+        )
+
         ev.msg.datapath.send_msg(flow_mod)
-        
-        print(GREEN + "Unlocked traffic on port %s of switch %s" + RESET , port_no, ev.msg.datapath.id)
-		
-		
-		
-		
-		
+
+        print(
+            GREEN + "Unlocked traffic on port %s of switch %s" + RESET,
+            port_no,
+            ev.msg.datapath.id,
+        )
 
     # Configurazione / Codice già fornito
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
@@ -237,35 +311,43 @@ class SimpleSwitch13(app_manager.RyuApp):
         parser = datapath.ofproto_parser
 
         match = parser.OFPMatch()
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
-                                          ofproto.OFPCML_NO_BUFFER)]
+        actions = [
+            parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)
+        ]
         self.add_flow(datapath, 0, match, actions)
 
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-                                             actions)]
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
         if buffer_id:
-            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
-                                    priority=priority, match=match,
-                                    instructions=inst)
+            mod = parser.OFPFlowMod(
+                datapath=datapath,
+                buffer_id=buffer_id,
+                priority=priority,
+                match=match,
+                instructions=inst,
+            )
         else:
-            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                    match=match, instructions=inst)
+            mod = parser.OFPFlowMod(
+                datapath=datapath, priority=priority, match=match, instructions=inst
+            )
         datapath.send_msg(mod)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         if ev.msg.msg_len < ev.msg.total_len:
-            self.logger.debug("packet truncated: only %s of %s bytes",
-                              ev.msg.msg_len, ev.msg.total_len)
+            self.logger.debug(
+                "packet truncated: only %s of %s bytes",
+                ev.msg.msg_len,
+                ev.msg.total_len,
+            )
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        in_port = msg.match['in_port']
+        in_port = msg.match["in_port"]
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
@@ -300,7 +382,11 @@ class SimpleSwitch13(app_manager.RyuApp):
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
 
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                  in_port=in_port, actions=actions, data=data)
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=data,
+        )
         datapath.send_msg(out)
-
